c27638c735049a91b34d7ef37ac445ea2020189b
riagan16 <68056300+riagan16@users.noreply.github.com>
2021-08-04 18:38:49 +0700

    Add files via upload

---
+++ Belajar 1/latihan1.js
@@ -0,0 +1,15 @@
+/*alert('Welcome kamu dengan kesulitan ini!');
+var nama = prompt('masukkan nama');1
+document.write('Selamat datang kak ' + nama + '<br>');
+document.write('Coba baru ini' + '<br>');
+document.write(5+5 + '<br>');*/
+/* var bil1 = parseInt(prompt('masukkan bil 1'));
+var bil2 = parseInt(prompt('masukan bil 2')); */
+
+var bil = 10;
+var bil2 = 20;
+var bil3 = bil1 + bil2;
+document.write('bil1:' bil1 + '<br>');
+document.write('bil2:' bil2 + '<br>');
+document.write(bil1 > bil2);
+document.write(bil < bil2);
---
+++ Belajar 1/latihan2.js
@@ -0,0 +1,7 @@
+var bil = 10;
+var bil2 = 20;
+var bil3 = bil1 + bil2;
+document.write('bil1:' bil1 + '<br>');
+document.write('bil2:' bil2 + '<br>');
+document.write(bil1 > bil2);
+document.write(bil < bil2);
